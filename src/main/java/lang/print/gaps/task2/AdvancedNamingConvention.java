cb4a8ebf7430e9bc317766adc02de812c8a1eaeb
Marat <maratkudaibergenov@gmail.com>
2022-08-15 21:29:34 +0600

    corrected the wrong names

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,17 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+
+    private static final int adultAge = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    public static void CAllToFriend() {
+        //some code
+    }
+
+    public static void CallByNumber(int Number) {
+        //some code
+    }
+}
